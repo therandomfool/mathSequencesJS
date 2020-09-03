9bd79120779c972453ac992d0144a281f3394bcc
therandomfool <mddenver@protonmail.com>
2020-09-03 16:20:38 -0700

    mathsequences set up

---
+++ app.js
@@ -0,0 +1,15 @@
+// Arithmetic, Geoetric or no patterrr
+//  no negative numbers
+
+
+
+
+
+console.log(mathSequences([2,4,6,8]));
+// Arthmetric
+
+console.log(mathSequences([3,9,27]));
+// Geometric
+
+console.log(mathSequences([2,5,14,89]));
+// No Pattern
